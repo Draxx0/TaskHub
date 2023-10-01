579ecca9059ece8f501e67e3fceeb6890028d8b3
Draxx0 <william.fort.lmgl@gmail.com>
2023-10-02 00:43:38 +0200

    :fire: delete test code

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-import firebaseService from "./service/firebase.service";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  useEffect(() => {
-    firebaseService.getFirebaseCollection({
-      collection: "workshops",
-    });
-  }, []);
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
