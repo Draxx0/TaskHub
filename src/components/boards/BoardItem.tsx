2804d2bf72374bfa2c84226b4da30a522bb00460
DraxX0 <williamfort.lmgl@gmail.com>
2023-10-24 15:14:35 +0200

    :globe_with_meridians: add new translations

--- src/components/boards/BoardItem.tsx
+++ src/components/boards/BoardItem.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "../ui/button";
 import { formatDate } from "@/utils/functions/formatDate";
@@ -6,35 +13,49 @@ import { storedLang } from "@/main";
 import { useTranslation } from "react-i18next";
 import { Board } from "@/utils/types/board";
 
-
 const BoardItem = ({ board }: { board: Board }) => {
- const navigate = useNavigate()
- const { t } = useTranslation(["boards", "global"])
- const { id: workshopId } = useParams()
- return (
-  <Card className="hover:border-main-500 transition ease-in-out duration-300">
-   <CardHeader className="p-0">
-    <div className="p-4 flex flex-col gap-3">
-     <div className="flex items-center gap-2">
-      <div className="w-12 h-12 bg-main-500/30 flex items-center justify-center rounded-lg p-2">
-       <p className="font-bold text-main-500">{board.name[0]}</p>
-      </div>
-      <CardTitle className="capitalize">{board.name}</CardTitle>
-     </div>
-     <CardDescription className="truncate" title={board.description}>{board.description}</CardDescription>
-    </div>
-   </CardHeader>
-   <CardContent className="p-4">
-    <p>{t("workshop_card.board")} - <span className="text-main-500 font-semibold">4</span></p>
-   </CardContent>
-   <CardFooter className="flex p-4 justify-between">
-    <small> {t("workshop_card.date")} <span className="font-semibold">{formatDate({ date: board.createdAt, locale: storedLang || "fr" })}</span></small>
-    <Button variant={"outline"} onClick={() => navigate(`/workshops/${workshopId}/boardId/${board.id}`)}>
-     {t("global:button.select")}
-    </Button>
-   </CardFooter>
-  </Card>
- );
-}
+  const navigate = useNavigate();
+  const { t } = useTranslation(["boards", "global"]);
+  const { id: workshopId } = useParams();
+  return (
+    <Card className="hover:border-main-500 transition ease-in-out duration-300">
+      <CardHeader className="p-0">
+        <div className="p-4 flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <div className="w-12 h-12 bg-main-500/30 flex items-center justify-center rounded-lg p-2">
+              <p className="font-bold text-main-500">{board.name[0]}</p>
+            </div>
+            <CardTitle className="capitalize">{board.name}</CardTitle>
+          </div>
+          <CardDescription className="truncate" title={board.description}>
+            {board.description}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="p-4">
+        <p>
+          {t("workshop_card.board")} -{" "}
+          <span className="text-main-500 font-semibold">4</span>
+        </p>
+      </CardContent>
+      <CardFooter className="flex p-4 justify-between">
+        <small>
+          {t("global:date")}{" "}
+          <span className="font-semibold">
+            {formatDate({ date: board.createdAt, locale: storedLang || "fr" })}
+          </span>
+        </small>
+        <Button
+          variant={"outline"}
+          onClick={() =>
+            navigate(`/workshops/${workshopId}/boardId/${board.id}`)
+          }
+        >
+          {t("global:button.select")}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default BoardItem;
--- src/components/workshops/WorkshopsItem.tsx
+++ src/components/workshops/WorkshopsItem.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Workshop } from "@/utils/types/workshop";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
@@ -6,35 +13,55 @@ import { formatDate } from "@/utils/functions/formatDate";
 import { storedLang } from "@/main";
 import { useTranslation } from "react-i18next";
 
-
 const WorkshopsItem = ({ workshop }: { workshop: Workshop }) => {
- const navigate = useNavigate()
- const { t } = useTranslation(["workshops", "global"])
- return (
-  <Card className="hover:border-main-500 transition ease-in-out duration-300">
-   <CardHeader className="p-0">
-    <img src={workshop.coverUrl} className="h-40 object-cover rounded-t-lg" alt="" />
-    <div className="p-4 flex flex-col gap-3">
-     <div className="flex items-center gap-2">
-      <div className="w-12 h-12 bg-main-500/30 flex items-center justify-center rounded-lg p-2">
-       <p className="font-bold text-main-500">{workshop.name[0]}</p>
-      </div>
-      <CardTitle className="capitalize">{workshop.name}</CardTitle>
-     </div>
-     <CardDescription className="truncate" title={workshop.description}>{workshop.description}</CardDescription>
-    </div>
-   </CardHeader>
-   <CardContent className="p-4">
-    <p>{t("workshop_card.board")} - <span className="text-main-500 font-semibold">4</span></p>
-   </CardContent>
-   <CardFooter className="flex p-4 justify-between">
-    <small> {t("workshop_card.date")} <span className="font-semibold">{formatDate({ date: workshop.createdAt, locale: storedLang || "fr" })}</span></small>
-    <Button variant={"outline"} onClick={() => navigate(`/workshops/${workshop.id}`)}>
-     {t("global:button.select")}
-    </Button>
-   </CardFooter>
-  </Card>
- );
-}
+  const navigate = useNavigate();
+  const { t } = useTranslation(["workshops", "global"]);
+  return (
+    <Card className="hover:border-main-500 transition ease-in-out duration-300">
+      <CardHeader className="p-0">
+        <img
+          src={workshop.coverUrl}
+          className="h-40 object-cover rounded-t-lg"
+          alt=""
+        />
+        <div className="p-4 flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <div className="w-12 h-12 bg-main-500/30 flex items-center justify-center rounded-lg p-2">
+              <p className="font-bold text-main-500">{workshop.name[0]}</p>
+            </div>
+            <CardTitle className="capitalize">{workshop.name}</CardTitle>
+          </div>
+          <CardDescription className="truncate" title={workshop.description}>
+            {workshop.description}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="p-4">
+        <p>
+          {t("workshop_card.board")} -{" "}
+          <span className="text-main-500 font-semibold">4</span>
+        </p>
+      </CardContent>
+      <CardFooter className="flex p-4 justify-between">
+        <small>
+          {" "}
+          {t("global:date")}{" "}
+          <span className="font-semibold">
+            {formatDate({
+              date: workshop.createdAt,
+              locale: storedLang || "fr",
+            })}
+          </span>
+        </small>
+        <Button
+          variant={"outline"}
+          onClick={() => navigate(`/workshops/${workshop.id}`)}
+        >
+          {t("global:button.select")}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default WorkshopsItem;
